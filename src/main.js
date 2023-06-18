ca1cc58c441efee26ee421949114cfb972f29de6
Chan0818 <38553336@qq.com>
2023-06-19 01:21:50 +0800

    navigation部分事件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './styles/index.scss'
 import { useREM } from './utils/flexible'
+import mLibs from './libs'
+import 'virtual:svg-icons-register'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(mLibs).use(router).mount('#app')
 useREM()
--- src/views/main/component/navigation/mobile/index.vue
+++ src/views/main/component/navigation/mobile/index.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="bg-white sticky top-0 left-0 z-10">
     <ul class="relative flex overflow-x-auto 
-        p-1 text-zinc-600 overflow-hidden">
-        <!-- categoryitem -->
+        p-1 text-xs text-zinc-600 overflow-hidden"
+        ref="ulTarget">
+      <!-- siler处理 -->
+      <li ref="sliderTarget"
+          class="absolute bg-zinc-900 rounded-lg duration-200 h-[22px] "
+          :style="sliderStyle"></li>
+      <!-- 汉堡按钮 -->
+      <li
+        class="z-20 bg-white fixed top-0 right-[-1px] 
+         h-4 px-1 flex items-center shadow-l-white"
+      ><m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon></li>
+      <!-- categoryitem -->
       <li v-for="(item,index) in data" :key="item.id"
-          class="shrink-0 px-1.5 py-0.5 z-10 duration-200">
+          :ref="setItemRef"
+          @click="onItemClick(index)"
+          class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4">
           {{ item.name }}
       </li>
     </ul>
@@ -12,12 +24,45 @@
 </template>
 
 <script setup>
+import { ref, watch, onBeforeUpdate } from 'vue'
+import { useScroll } from '@vueuse/core'
 defineProps({
   data:{
     type:Array,
     required:true
   }
 })
+//滑块处理
+  const sliderStyle = ref({
+    transform:'translateX(0px)',
+    width:'52px'
+  })
+  //选中的item下标
+  const currentCategoryIndex =ref(0)
+  //获取填充的所有item元素
+  let itemRefs = []
+  const setItemRef = (el) => {
+    if(el){
+      itemRefs.push(el)
+    }
+  }
+  onBeforeUpdate(()=>{
+    itemRefs = []
+  })
+//获取ul元素,以计算偏移量
+const ulTarget = ref(null)
+const {x:ulScrollLeft} = useScroll(ulTarget)
+watch(currentCategoryIndex,(val)=>{
+  //获取选中元素的left,width
+  const { left,width } = itemRefs[val].getBoundingClientRect()
+  //sliderstyle
+  sliderStyle.value = {
+    //ul+left
+    transform:`translateX(${ulScrollLeft.value + left - 10 +'px'})`,
+    width:width +'px'
+  }
+})
+
 </script>
 <style scoped lang='scss'>
 </style>
